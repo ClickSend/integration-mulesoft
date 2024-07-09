dd3f97fee87ef2ee87c108291ddd64b881de2a9c
Vaibhav Khanna <vaibhavkhanna024@gmail.com>
2024-07-09 16:53:20 +0530

    Removing extra errorTypeProvider class

--- src/main/java/org/clicksend/internal/error/provider/MessageErrorTypeProvider.java
+++ src/main/java/org/clicksend/internal/error/provider/MessageErrorTypeProvider.java
@@ -3,19 +3,21 @@
  */
 package org.clicksend.internal.error.provider;
 
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.clicksend.internal.error.MessageErrorType;
+import org.mule.runtime.extension.api.annotation.error.ErrorTypeProvider;
 import org.mule.runtime.extension.api.error.ErrorTypeDefinition;
 
+public class MessageErrorTypeProvider implements ErrorTypeProvider {
 
-	public class MessageErrorTypeProvider extends ExecutionErrorTypeProvider {
-
-	    @Override
-	    public Set<ErrorTypeDefinition<MessageErrorType>> getMessageErrorTypes() {
-	        Set<ErrorTypeDefinition<MessageErrorType>> errors = super.getMessageErrorTypes();
-	        errors.add(MessageErrorType.JSON_PARSER_EXCEPTION);
-	        return errors;
-	    }
-
+	@Override
+	public Set<ErrorTypeDefinition> getErrorTypes() {
+		HashSet<ErrorTypeDefinition> errors = new HashSet<>();
+		errors.addAll((Collection) EnumSet.allOf(MessageErrorType.class));
+		return errors;
+	}
 }
